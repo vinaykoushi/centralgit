a871f411bf8036dc067b57fa2ff8befc791ec248
vinayakarashanagi17 <96831752+vinaykoushi@users.noreply.github.com>
2025-07-10 11:07:06 +0530

    Create virtualbox-setup.sh

---
+++ virtualbox-setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+
+sudo apt-get update -y
+sudo apt-get install conntrack -y
+sudo apt-get install curl apt-transport-https wget -y
+sudo apt-get install virtualbox virtualbox-ext-pack -y
